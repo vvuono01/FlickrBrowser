f264f5cab09977b59ec87215b8a7da1ca0a50b74
Vince Vuono <vvuono01@gmail.com>
2018-07-29 13:24:16 -0400

    Set up the presenter to be injected

--- app/src/main/java/com/vvuono/flickrbrowser/presenter/BrowserPresenter.java
+++ app/src/main/java/com/vvuono/flickrbrowser/presenter/BrowserPresenter.java
@@ -2,10 +2,15 @@ package com.vvuono.flickrbrowser.presenter;
 
 import com.vvuono.flickrbrowser.contracts.BrowserContract;
 
+import javax.inject.Inject;
+
 public class BrowserPresenter extends BasePresenter<BrowserContract.View>
         implements BrowserContract.Presenter {
+    @Inject
+    public BrowserPresenter() {}
+
     @Override
     public void loadImages() {
-        getView().onImagesLoaded("TEST DATA");
+        getView().onImagesLoaded("TEST PRESENTER DATA");
     }
 }
