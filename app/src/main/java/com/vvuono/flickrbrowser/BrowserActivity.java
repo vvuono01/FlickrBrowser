f9b8dae643aca4fc84b999871e4ddf362c53eb4f
Vince Vuono <vvuono01@gmail.com>
2018-07-29 11:58:41 -0400

    Set up the BrowserActivity to display the test data loaded by the presenter

--- app/src/main/java/com/vvuono/flickrbrowser/BrowserActivity.java
+++ app/src/main/java/com/vvuono/flickrbrowser/BrowserActivity.java
@@ -1,13 +1,40 @@
 package com.vvuono.flickrbrowser;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
 
-public class BrowserActivity extends AppCompatActivity {
+import com.vvuono.flickrbrowser.contracts.BrowserContract;
+import com.vvuono.flickrbrowser.presenter.BrowserPresenter;
+import com.vvuono.flickrbrowser.view.BaseActivity;
+
+import butterknife.BindView;
+
+public class BrowserActivity extends BaseActivity implements BrowserContract.View {
+    @BindView(R.id.tv_test) TextView mTestText;
+
+    private BrowserPresenter mPresenter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_browser);
+    protected int getContentResource() {
+        return R.layout.activity_browser;
+    }
+
+    @Override
+    protected void init(@Nullable Bundle state) {
+        mPresenter = new BrowserPresenter(); // TODO: Replace this with dependency injection
+        mPresenter.attach(this);
+        mPresenter.loadImages();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mPresenter.detach();
+    }
+
+    @Override
+    public void onImagesLoaded(String imageData) {
+        mTestText.setText(imageData);
     }
 }
--- app/src/main/java/com/vvuono/flickrbrowser/view/BaseActivity.java
+++ app/src/main/java/com/vvuono/flickrbrowser/view/BaseActivity.java
@@ -5,11 +5,14 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+
 public abstract class BaseActivity extends AppCompatActivity implements BaseView {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getContentResource());
+        ButterKnife.bind(this);
         init(savedInstanceState);
     }
 
